1199ba1147075185ba219b3f4c80b4d426cf47bb
Ashley King <Princessak8918@hotmail.com>
2023-03-24 19:14:56 -0600

    pass test

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form')
+  let newtask = document.getElementById('new-task-description')
+  const tasks = document.getElementById('tasks')
+
+  console.log(form)
+
+  form.addEventListener('submit', function(e){
+    e.preventDefault()
+    let taskToAdd = document.createElement('p')
+    let removeButton = document.createElement('button')
+    removeButton.textContent = ('X')
+    removeButton.addEventListener('click', () => taskToAdd.remove())
+    taskToAdd.textContent = newtask.value
+    tasks.append(taskToAdd)
+    taskToAdd.append(removeButton)
+
+  })
+
+
+
 });
+ 
+
